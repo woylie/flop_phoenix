6c3a9dc49adfcd3dd5bf5fd4d1ff65a2d339620f
Mathias Polligkeit <git@woylie.de>
2020-06-15 09:56:41 +0200

    add page links

--- lib/flop_phoenix.ex
+++ lib/flop_phoenix.ex
@@ -30,7 +30,8 @@ defmodule FlopPhoenix do
       aria: [label: "pagination"] do
       [
         previous_link(meta, page_link_helper, opts),
-        next_link(meta, page_link_helper, opts)
+        next_link(meta, page_link_helper, opts),
+        page_links(meta, page_link_helper, opts)
       ]
     end
   end
@@ -63,6 +64,37 @@ defmodule FlopPhoenix do
     else
       content_tag :span, class: link_class, disabled: "disabled" do
         content
+      end
+    end
+  end
+
+  @spec page_links(Meta.t(), function, keyword) :: Phoenix.HTML.safe()
+  def page_links(meta, route_func, opts \\ []) do
+    aria_label = opts[:pagination_link_aria_label] || (&"Goto page #{&1}")
+    link_class = opts[:pagination_link_class] || "pagination-link"
+    list_class = opts[:pagination_list_class] || "pagination-list"
+
+    content_tag :ul, class: list_class do
+      for page <- 1..meta.total_pages do
+        class =
+          if meta.current_page == page,
+            do: "#{link_class} is-current",
+            else: link_class
+
+        common_aria = [label: aria_label.(page)]
+
+        aria =
+          if meta.current_page == page,
+            do: [{:current, "page"} | common_aria],
+            else: common_aria
+
+        content_tag :li do
+          link(page,
+            to: route_func.(page),
+            aria: aria,
+            class: class
+          )
+        end
       end
     end
   end
--- test/flop_phoenix_test.exs
+++ test/flop_phoenix_test.exs
@@ -5,9 +5,17 @@ defmodule FlopPhoenixTest do
   import FlopPhoenix
   import FlopPhoenix.Factory
 
+  alias Flop.Meta
+
   doctest FlopPhoenix
 
   @route_helper_opts [%{}, :pets]
+
+  defp render_pagination(%Meta{} = meta, opts \\ []) do
+    meta
+    |> pagination(&route_helper/3, @route_helper_opts, opts)
+    |> safe_to_string()
+  end
 
   defp route_helper(%{}, path, query) do
     URI.to_string(%URI{path: "/#{path}", query: URI.encode_query(query)})
@@ -15,10 +23,7 @@ defmodule FlopPhoenixTest do
 
   describe "pagination/4" do
     test "renders pagination wrapper" do
-      result =
-        build(:meta)
-        |> pagination(&route_helper/3, @route_helper_opts)
-        |> safe_to_string()
+      result = render_pagination(build(:meta))
 
       assert String.starts_with?(
                result,
@@ -29,199 +34,255 @@ defmodule FlopPhoenixTest do
     end
 
     test "does not render anything if there is only one page" do
-      meta =
-        build(:meta,
-          total_pages: 1,
-          has_previous_page?: false,
-          has_next_page?: false
-        )
-
       result =
-        meta
-        |> pagination(&route_helper/3, @route_helper_opts)
-        |> safe_to_string()
+        render_pagination(
+          build(:meta,
+            total_pages: 1,
+            has_previous_page?: false,
+            has_next_page?: false
+          )
+        )
 
       assert result == ""
     end
 
     test "allows to overwrite wrapper class" do
-      result =
-        build(:meta)
-        |> pagination(&route_helper/3, @route_helper_opts, wrapper_class: "boo")
-        |> safe_to_string()
+      result = render_pagination(build(:meta), wrapper_class: "boo")
 
       assert result =~
                "<nav aria-label=\"pagination\" class=\"boo\" role=\"navigation\">"
     end
 
     test "renders previous link" do
-      meta =
-        build(:meta,
-          current_page: 2,
-          current_offset: 10,
-          has_previous_page?: true,
-          previous_page: 1,
-          previous_offset: 0
-        )
-
       result =
-        meta
-        |> pagination(&route_helper/3, @route_helper_opts)
-        |> safe_to_string()
+        render_pagination(
+          build(:meta,
+            current_page: 2,
+            current_offset: 10,
+            has_previous_page?: true,
+            previous_page: 1,
+            previous_offset: 0
+          )
+        )
 
       assert result =~
                "<a class=\"pagination-previous\" href=\"/pets?page=1&amp;page_size=10\">Previous</a>"
     end
 
     test "allows to overwrite previous link class and label" do
-      meta =
-        build(:meta,
-          current_page: 2,
-          current_offset: 10,
-          has_previous_page?: true,
-          previous_page: 1,
-          previous_offset: 0
-        )
-
       result =
-        meta
-        |> pagination(&route_helper/3, @route_helper_opts,
+        render_pagination(
+          build(:meta,
+            current_page: 2,
+            current_offset: 10,
+            has_previous_page?: true,
+            previous_page: 1,
+            previous_offset: 0
+          ),
           previous_link_class: "prev",
           previous_link_content:
             content_tag :i, class: "fas fa-chevron-left" do
             end
         )
-        |> safe_to_string()
 
       assert result =~
                "<a class=\"prev\" href=\"/pets?page=1&amp;page_size=10\"><i class=\"fas fa-chevron-left\"></i></a>"
     end
 
     test "disables previous link if on first page" do
-      meta =
-        build(:meta,
-          current_offset: 0,
-          current_page: 1,
-          has_previous_page?: false,
-          previous_offset: nil,
-          previous_page: nil
-        )
-
       result =
-        meta
-        |> pagination(&route_helper/3, @route_helper_opts)
-        |> safe_to_string()
+        render_pagination(
+          build(:meta,
+            current_offset: 0,
+            current_page: 1,
+            has_previous_page?: false,
+            previous_offset: nil,
+            previous_page: nil
+          )
+        )
 
       assert result =~
                "<span class=\"pagination-previous\" disabled=\"disabled\">Previous</span>"
     end
 
     test "allows to overwrite previous link class and label if disabled" do
-      meta =
-        build(:meta,
-          current_offset: 0,
-          current_page: 1,
-          has_previous_page?: false,
-          previous_offset: nil,
-          previous_page: nil
-        )
-
       result =
-        meta
-        |> pagination(&route_helper/3, @route_helper_opts,
+        render_pagination(
+          build(:meta,
+            current_offset: 0,
+            current_page: 1,
+            has_previous_page?: false,
+            previous_offset: nil,
+            previous_page: nil
+          ),
           previous_link_class: "prev",
           previous_link_content: "Prev"
         )
-        |> safe_to_string()
 
       assert result =~
                "<span class=\"prev\" disabled=\"disabled\">Prev</span>"
     end
 
     test "renders next link" do
-      meta =
-        build(:meta,
-          current_page: 2,
-          current_offset: 10,
-          has_next_page?: true,
-          next_page: 3,
-          next_offset: 20
-        )
-
       result =
-        meta
-        |> pagination(&route_helper/3, @route_helper_opts)
-        |> safe_to_string()
+        render_pagination(
+          build(:meta,
+            current_page: 2,
+            current_offset: 10,
+            has_next_page?: true,
+            next_page: 3,
+            next_offset: 20
+          )
+        )
 
       assert result =~
                "<a class=\"pagination-next\" href=\"/pets?page=3&amp;page_size=10\">Next</a>"
     end
 
     test "allows to overwrite next link class and label" do
-      meta =
-        build(:meta,
-          current_page: 2,
-          current_offset: 10,
-          has_next_page?: true,
-          next_page: 3,
-          next_offset: 20
-        )
-
       result =
-        meta
-        |> pagination(&route_helper/3, @route_helper_opts,
+        render_pagination(
+          build(:meta,
+            current_page: 2,
+            current_offset: 10,
+            has_next_page?: true,
+            next_page: 3,
+            next_offset: 20
+          ),
           next_link_class: "next",
           next_link_content:
             content_tag :i, class: "fas fa-chevron-right" do
             end
         )
-        |> safe_to_string()
 
       assert result =~
                "<a class=\"next\" href=\"/pets?page=3&amp;page_size=10\"><i class=\"fas fa-chevron-right\"></i></a>"
     end
 
     test "disables next link if on last page" do
-      meta =
-        build(:meta,
-          current_offset: 40,
-          current_page: 5,
-          has_next_page?: false,
-          next_offset: nil,
-          next_page: nil
-        )
-
       result =
-        meta
-        |> pagination(&route_helper/3, @route_helper_opts)
-        |> safe_to_string()
+        render_pagination(
+          build(:meta,
+            current_offset: 40,
+            current_page: 5,
+            has_next_page?: false,
+            next_offset: nil,
+            next_page: nil
+          )
+        )
 
       assert result =~
                "<span class=\"pagination-next\" disabled=\"disabled\">Next</span>"
     end
 
     test "allows to overwrite next link class and label when disabled" do
-      meta =
-        build(:meta,
-          current_offset: 40,
-          current_page: 5,
-          has_next_page?: false,
-          next_offset: nil,
-          next_page: nil
-        )
-
       result =
-        meta
-        |> pagination(&route_helper/3, @route_helper_opts,
+        render_pagination(
+          build(:meta,
+            current_offset: 40,
+            current_page: 5,
+            has_next_page?: false,
+            next_offset: nil,
+            next_page: nil
+          ),
           next_link_class: "next",
           next_link_content:
             content_tag :i, class: "fas fa-chevron-right" do
             end
         )
-        |> safe_to_string()
 
       assert result =~
                "<span class=\"next\" disabled=\"disabled\"><i class=\"fas fa-chevron-right\"></i></span>"
+    end
+
+    test "renders page links" do
+      result =
+        render_pagination(
+          build(:meta,
+            current_page: 2,
+            has_next_page?: true,
+            has_previous_page?: true,
+            next_page: 3,
+            page_size: 10,
+            previous_page: 1,
+            total_pages: 3
+          )
+        )
+
+      assert result =~ "<ul class=\"pagination-list\">"
+
+      assert result =~
+               "<li><a aria-label=\"Goto page 1\" class=\"pagination-link\" href=\"/pets?page=1&amp;page_size=10\">1</a></li>"
+
+      assert result =~
+               "<li><a aria-current=\"page\" aria-label=\"Goto page 2\" class=\"pagination-link is-current\" href=\"/pets?page=2&amp;page_size=10\">2</a></li>"
+
+      assert result =~
+               "<li><a aria-label=\"Goto page 3\" class=\"pagination-link\" href=\"/pets?page=3&amp;page_size=10\">3</a></li>"
+
+      assert result =~ "</ul>"
+    end
+
+    test "allows to overwrite pagination list class" do
+      result =
+        render_pagination(
+          build(:meta,
+            current_page: 2,
+            has_next_page?: true,
+            has_previous_page?: true,
+            next_page: 3,
+            page_size: 10,
+            previous_page: 1,
+            total_pages: 3
+          ),
+          pagination_list_class: "p-list"
+        )
+
+      assert result =~ "<ul class=\"p-list\">"
+    end
+
+    test "allows to overwrite pagination link class" do
+      result =
+        render_pagination(
+          build(:meta,
+            current_page: 2,
+            has_next_page?: true,
+            has_previous_page?: true,
+            next_page: 3,
+            page_size: 10,
+            previous_page: 1,
+            total_pages: 3
+          ),
+          pagination_link_class: "p-link"
+        )
+
+      assert result =~
+               "<li><a aria-label=\"Goto page 1\" class=\"p-link\" href=\"/pets?page=1&amp;page_size=10\">1</a></li>"
+
+      assert result =~
+               "<li><a aria-current=\"page\" aria-label=\"Goto page 2\" class=\"p-link is-current\" href=\"/pets?page=2&amp;page_size=10\">2</a></li>"
+    end
+
+    test "allows to overwrite pagination link aria label" do
+      result =
+        render_pagination(
+          build(:meta,
+            current_page: 2,
+            has_next_page?: true,
+            has_previous_page?: true,
+            next_page: 3,
+            page_size: 10,
+            previous_page: 1,
+            total_pages: 3
+          ),
+          pagination_link_aria_label: &"On to page #{&1}"
+        )
+
+      assert result =~
+               "<li><a aria-label=\"On to page 1\" class=\"pagination-link\" href=\"/pets?page=1&amp;page_size=10\">1</a></li>"
+
+      assert result =~
+               "<li><a aria-current=\"page\" aria-label=\"On to page 2\" class=\"pagination-link is-current\" href=\"/pets?page=2&amp;page_size=10\">2</a></li>"
     end
   end
 end
