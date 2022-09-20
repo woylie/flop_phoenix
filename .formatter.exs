4bccd1b6de081e5290247de4293d885f0ca55b52
Mathias Polligkeit <git@woylie.de>
2022-09-21 07:16:16 +0900

    update formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -3,9 +3,13 @@
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   line_length: 80,
-  import_deps: [:stream_data],
+  import_deps: [:phoenix_live_view, :stream_data],
+  # elixir_ls refuses to import deps... mirror live view config to mitigate
   locals_without_parens: [
     attr: 2,
-    attr: 3
+    attr: 3,
+    slot: 1,
+    slot: 2,
+    slot: 3
   ]
 ]
