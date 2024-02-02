50ebfd8469017828a5e86d30cc673c0a439eaef8
Mathias Polligkeit <git@woylie.de>
2024-02-02 23:51:37 +0900

    add test helpers

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -11,12 +11,6 @@ defmodule Flop.Phoenix.TestHelpers do
 
   alias Plug.Conn.Query
 
-  def render_form(assigns) do
-    (&filter_form/1)
-    |> render_component(assigns)
-    |> Floki.parse_fragment!()
-  end
-
   @doc """
   Renders the given HEEx template and parses it with Floki.
 
@@ -27,6 +21,79 @@ defmodule Flop.Phoenix.TestHelpers do
   def parse_heex(heex) do
     heex
     |> rendered_to_string()
+    |> Floki.parse_fragment!()
+  end
+
+  @doc """
+  Returns the trimmed text nodes from the first level of the HTML tree.
+  """
+  def text(html) do
+    html
+    |> Floki.text(deep: false)
+    |> String.trim()
+  end
+
+  @doc """
+  Returns the trimmed text nodes from the first level of the HTML tree returned
+  by the selector.
+
+  Raises if the selector returns zero or more than one result.
+  """
+  def text(html, selector) do
+    html
+    |> find_one(selector)
+    |> Floki.text(deep: false)
+    |> String.trim()
+  end
+
+  @doc """
+  Wrapper around `Floki.attribute/2` that unwraps the single attribute.
+
+  Raises if the attribute occurs multiple times.
+  """
+  def attribute(html, name) do
+    case Floki.attribute(html, name) do
+      [value] -> value
+      [] -> nil
+      _ -> raise "found attribute #{name} multiple times"
+    end
+  end
+
+  @doc """
+  Wrapper around `Floki.attribute/3` that unwraps the single attribute.
+
+  Raises if the attribute occurs multiple times.
+  """
+  def attribute(html, selector, name) do
+    html
+    |> find_one(selector)
+    |> attribute(name)
+  end
+
+  def find_one(html, selector) do
+    case Floki.find(html, selector) do
+      [inner_html] ->
+        inner_html
+
+      [] ->
+        raise """
+        Selector #{inspect(selector)} did not return any results in:
+
+        #{inspect(html, pretty: true)}
+        """
+
+      [_ | _] = results ->
+        raise """
+        Selector #{inspect(selector)} returned multiple results:
+
+        #{inspect(results, pretty: true)}
+        """
+    end
+  end
+
+  def render_form(assigns) do
+    (&filter_form/1)
+    |> render_component(assigns)
     |> Floki.parse_fragment!()
   end
 
